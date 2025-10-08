3a5300887f78c5fab002dc93da15e108c746c982
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-10-08 23:29:21 +0100

    week2assignmentaddedp2

--- week-2/practice-2.rs
+++ week-2/practice-2.rs
@@ -6,6 +6,6 @@ fn main() {
     
     println!("format {} arguments", "some"); // prints format some arguments
     
-    println!("My name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo");
+    println!("My name is {}. I am the assistant class rep of stream 1", "Chifumnaya Jasper Omefe");
     
 }
