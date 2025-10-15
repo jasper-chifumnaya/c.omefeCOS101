1e46dfc08aaeb03e66a4c3e4585e536db5a23bb7
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-10-15 13:31:23 +0100

    practice_2

--- week-2/PROJECTS/project_2_sum_and_average.rs
+++ week-2/PROJECTS/project_2_sum_and_average.rs
@@ -29,3 +29,31 @@ fn main() {
     println!("    ½    ");
     println!("Average Sales: ₦{:.2}", average);
 }
+
+
+fn main() {
+    // Sales amounts for each item
+    let tos = 450_000.0;
+
+    let mac = 1_500_000.0;
+
+    let hp = 750_000.0;
+
+    let del = 2_850_000.0;
+
+    let ace = 250_000.0;
+
+    let amounts = [tos, mac, hp, del, ace];
+
+    // sum
+    let sum: f64 = amounts.iter().sum();
+
+    //the average
+    let average = sum / amounts.len() as f64;
+
+    // Print
+
+    println!("Total Sales: ₦{:.2}", sum);
+
+    println!("Average Sales: ₦{:.2}", average);
+}
