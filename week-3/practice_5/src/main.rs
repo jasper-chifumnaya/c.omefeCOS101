22afc3415d58a966e091f6c6f71194489984ff1f
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-10-15 13:47:47 +0100

    practice5_week-3

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = 'B';
+    let surname = "Omefe";
+    let first_name = "Chifumnaya";
+    let middle_name = "Jasper";
+
+    println!(" ");
+    println!("Special character: {}",special_character);
+    println!("Alphabet: {}",alphabet);
+    println!("Name: {} {} {}",surname, middle_name, first_name);
+}
