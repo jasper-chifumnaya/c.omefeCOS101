93215e4187d0212379b452db0308ebd51b2b84d2
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-10-22 13:33:05 +0100

    practice3

---
+++ week-4/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\c.omefeCOS101\week-4\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+C:\Users\New\Documents\c.omefeCOS101\week-4\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+src\main.rs:
