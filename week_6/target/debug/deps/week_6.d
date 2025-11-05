ac571f69024f6d2c38f391c6fc3417bd4bca331c
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-11-05 13:16:26 +0100

    week 6

---
+++ week_6/target/debug/deps/week_6.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\c.omefeCOS101\week_6\target\debug\deps\week_6.d: src\main.rs
+
+C:\Users\New\Documents\c.omefeCOS101\week_6\target\debug\deps\week_6.exe: src\main.rs
+
+src\main.rs:
---
+++ week_6/target/debug/week_6.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\c.omefeCOS101\week_6\target\debug\week_6.exe: C:\Users\New\Documents\c.omefeCOS101\week_6\src\main.rs
