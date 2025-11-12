d5f9ae4f77bd53885e0fe74a031abafe96866419
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-11-12 17:21:36 +0100

    withloop

---
+++ week-7/practice_8/withloop/target/debug/deps/withloop.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\c.omefeCOS101\week-7\practice_8\withloop\target\debug\deps\withloop.d: src\main.rs
+
+C:\Users\New\Documents\c.omefeCOS101\week-7\practice_8\withloop\target\debug\deps\withloop.exe: src\main.rs
+
+src\main.rs:
---
+++ week-7/practice_8/withloop/target/debug/withloop.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\c.omefeCOS101\week-7\practice_8\withloop\target\debug\withloop.exe: C:\Users\New\Documents\c.omefeCOS101\week-7\practice_8\withloop\src\main.rs
