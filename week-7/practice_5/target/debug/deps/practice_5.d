751b1b6a28fd96850174c00eff94c3347d08d2d0
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-11-12 16:29:58 +0100

    practice5

---
+++ week-7/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\c.omefeCOS101\week-7\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\New\Documents\c.omefeCOS101\week-7\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
