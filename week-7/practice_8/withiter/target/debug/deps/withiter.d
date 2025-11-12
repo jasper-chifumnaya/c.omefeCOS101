afc822620aa133bb7891a2c21ca05f75d2251e59
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-11-12 17:25:53 +0100

    withiter

---
+++ week-7/practice_8/withiter/target/debug/deps/withiter.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\c.omefeCOS101\week-7\practice_8\withiter\target\debug\deps\withiter.d: src\main.rs
+
+C:\Users\New\Documents\c.omefeCOS101\week-7\practice_8\withiter\target\debug\deps\withiter.exe: src\main.rs
+
+src\main.rs:
---
+++ week-7/practice_8/withiter/target/debug/withiter.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\c.omefeCOS101\week-7\practice_8\withiter\target\debug\withiter.exe: C:\Users\New\Documents\c.omefeCOS101\week-7\practice_8\withiter\src\main.rs
