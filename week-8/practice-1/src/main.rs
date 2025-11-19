4e41b3155ba66a365649f1302f4e0b875ad162a6
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-11-19 12:47:51 +0100

    creating a vector

---
+++ week-8/practice-1/src/main.rs
@@ -0,0 +1,11 @@
+fn main(){
+
+let v : Vec<i64> = Vec::new();
+
+println!("\nThe length of Vec::new is: {}",v.len() );
+
+let v = vec!["Grace", "Tom", "Isha", "Belle", "Chuck"];
+
+println!("\nThe length of Vec macro is: {}",v.len() );
+
+}
---
+++ week-8/practice-1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\c.omefeCOS101\week-8\practice-1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\New\Documents\c.omefeCOS101\week-8\practice-1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
