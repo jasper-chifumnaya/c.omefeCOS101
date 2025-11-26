baecfe4a9225e4581dce6a5b025ce613a7c18705
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-11-26 13:06:23 +0100

    appended files

--- week-9/practice-4/src/main.rs
+++ week-9/practice-4/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = OpenOptions::new().append(true).open("../practice-2/welcome_message.txt").expect("cannot open file");
+    file.write_all("\nHello Class".as_bytes()).expect("write failed");
+    file.write_all("\nThis is the appendage to the document.".as_bytes()).expect("write failed");
+
+    println!("file append success");
 }
