b5111c66607054a047af58c7138610d50275c371
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-11-26 13:04:22 +0100

    read from a file

--- week-9/practice-2/src/main.rs
+++ week-9/practice-2/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::Read;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut file = std::fs::File::create("welcome_message.txt").expect("create failed");
+    let mut file = std::fs::File::open("welcome_message.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    print!("{}", contents);
 }
