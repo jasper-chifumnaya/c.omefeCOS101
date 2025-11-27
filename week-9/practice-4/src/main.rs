44dcf095811384fc28a9b83e959db89a338f9261
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-11-27 13:33:44 +0100

    fixed my copy

--- week-9/practice-4/src/main.rs
+++ week-9/practice-4/src/main.rs
@@ -2,6 +2,8 @@ use std::fs::OpenOptions;
 use std::io::Write;
 
 fn main() {
+    let table = "Lager\t\tStout\t\tNon-alcoholic\n33 Export\t\tLegend\t\tMaltina";
+
     let mut file = OpenOptions::new().append(true).open("../practice-2/welcome_message.txt").expect("cannot open file");
     file.write_all("\nHello Class".as_bytes()).expect("write failed");
     file.write_all("\nThis is the appendage to the document.".as_bytes()).expect("write failed");
