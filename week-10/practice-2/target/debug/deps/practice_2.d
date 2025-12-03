8afef272f9b33b57b5ac61141ed93d6a0e713427
Chifumnaya Jasper Omefe <chifumnaya.omefe@pau.edu.ng>
2025-12-03 13:15:20 +0100

    passing value to a function

---
+++ week-10/practice-2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\c.omefeCOS101\week-10\practice-2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\New\Documents\c.omefeCOS101\week-10\practice-2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
